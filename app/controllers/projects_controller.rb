184eb2714f4e7f9d33663559744e476d5970e409
tsubasa <tsubasa3612@gmail.com>
2017-10-07 01:08:52 +0000

    test

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -40,18 +40,33 @@ class ProjectsController < ApplicationController
     end
 
     def recipe
+
+        #カテゴリ取得APIを叩いてデータを取得する
+        #取得したカテゴリIDを取り出す
+        #取り出したカテゴリIDを並べる
+        #並べたカテゴリIDをカテゴリ別ランキングAPIのURLのcategoryId=のところに入れられる形に整える
+        #並べたカテゴリIDをカテゴリ別ランキングAPIのURLのcategoryId=のところに入れる
+        #カテゴリ別ランキングAPIを叩いて取得したデータをDBに保存する
+        #カテゴリ別ランキングの取得を全てのカテゴリIDで繰り返す
+
+        #カテゴリ別ランキング取得apiを叩いてjsonデータを取ってきてパースする
         uri = URI.parse('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&elements=recipiId%2CrecipeTitle%2CrecipeUrl%2CsmallImageUrl%2CrecipeMaterial&applicationId=1039136772752765175')
+        # uri = URI.parse('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&categoryId= + hoge + &elements=recipiId%2CrecipeTitle%2CrecipeUrl%2CsmallImageUrl%2CrecipeMaterial&applicationId=1039136772752765175')
         json = Net::HTTP.get(uri)
         results = JSON.parse(json)
         # binding.pry
 
-        #取得したjsonデータをDBに入れるための繰り返し処理
+        #パースしたjsonデータをDBに入れるための繰り返し処理
         results["result"].each do |r|
         # binding.pry
-            Recipe.create!(title: r["recipeTitle"], url: r["recipeUrl"], image: r["smallImageUrl"])
+            recipe = Recipe.create!(title: r["recipeTitle"], url: r["recipeUrl"], image: r["smallImageUrl"]) # recipe = の変数宣言がないとエラー吐く
+        # binding.pry
                 r["recipeMaterial"].each do |m|
         # binding.pry
-                    Material.create!(name: m, recipe_id: material.recipe._id)
+                    # recipe = Recipe.new
+        # binding.pry
+                    m = Material.create!(name: m, recipe_id: recipe.id)
+        # binding.pry
                 end
         end
 
